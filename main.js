5836e0efe27a9830eaf83d5006aa8ec69d589d49
codemode365 <codemode365@gmail.com>
2022-05-12 18:29:23 -0700

    clearing property added

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-const numericButn=Array.from(document.querySelectorAll('.num'));
-const functionalButton=Array.from(document.querySelectorAll('.fun'));
+const numericButn = Array.from(document.querySelectorAll('.num'));
+const functionalButton = Array.from(document.querySelectorAll('.fun'));
+
 
 alert("This project is current on development process")
-const ArraySotre = [...numericButn,...functionalButton]
-console.log(ArraySotre)
+const ArraySotre = [...numericButn, ...functionalButton]
+// console.log(ArraySotre)
 // console.log(screenTxt)
 // const screenTxt = document.querySelector('.screen-text').innerText;
 
-ArraySotre.forEach((currEle)=>{
-    currEle.addEventListener('click', ()=>{
-        //to be executed when equel to is pressed
-// const screenTxt = Number(document.querySelector('.screen-text').innerHTML);
-// const num1  = Number(currEle.innerHTML);
-// const screenData = screenTxt;
-// const NewValue = num1+screenData;
-// console.log(NewValue)
-// const screenValue = document.getElementById('value');
-
-// screenValue.innerText=NewValue;
-// console.log('working')
-// console.log(currEle.innerHTML)
+ArraySotre.forEach((currEle) => {
+    currEle.addEventListener('click', () => {
 
 
-//To add character on screen when clicked
-const screenTxt = document.querySelector('.screen-text').innerHTML;
-const screenValue = document.getElementById('value');
-const clickedChar = currEle.innerHTML;
-const finalText = screenTxt+clickedChar;
-screenValue.innerHTML=finalText
-
-})
+        //To show character on screen when clicked
+        const screenTxt = document.querySelector('.screen-text').innerHTML;
+        const screenValue = document.getElementById('value');
+        const clickedChar = currEle.innerHTML;
+        const finalText = screenTxt + clickedChar;
+        screenValue.innerHTML = finalText;
+        if (clickedChar == 'AC' || currEle.innerHTML == 'C') {
+            screenValue.innerHTML = '';
+        }
+        if(clickedChar == '='){
+            // let TOdelChar=clickedChar.replace(clickedChar.length-1, clickedChar.length);
+            // clickedChar=clickedChar-TOdelChar;
+            // clickedChar='';
+            console.log(TOdelChar)
+        }
+    
+    })
 })
