f37c559fe56c23947909b4b3bc4a0e53e106b856
codemode365 <codemode365@gmail.com>
2022-05-25 08:14:48 -0700

    calculation process completed

--- operation.js
+++ operation.js
@@ -14,27 +14,48 @@ function setScreen1(value) {
 function setScreen2(value) {
     document.getElementById('history').innerHTML = value;
 }
-function getScreen2(){
+function getScreen2() {
     return document.getElementById('history').innerText;
 }
 
-numericButn.forEach(thisEle=>{
-    thisEle.addEventListener('click',()=>{
-        const OnScreen1= getScreen1();
+numericButn.forEach(thisEle => {
+    thisEle.addEventListener('click', () => {
+        const OnScreen1 = getScreen1();
         const NewScreen1 = OnScreen1 + thisEle.innerText;
-        num1=NewScreen1;
+        num1 = NewScreen1;
         setScreen1(NewScreen1);
     })
 })
-functionalButton.forEach(thisEle=>{
-    thisEle.addEventListener('click',()=>{
-        num2=num1;
-        num1='';
-       setScreen2(num2+thisEle.innerText)
-       setScreen1('')
-       
+
+functionalButton.forEach(thisEle => {
+    thisEle.addEventListener('click', () => {
+        if (thisEle.innerHTML == '=') {
+            let output = eval(getScreen2() + getScreen1());
+            setScreen2(output);
+            setScreen1('');
+        }
+        else if (thisEle.innerHTML == 'AC') {
+            setScreen1('');
+            setScreen2('');
+        }
+        else if (thisEle.innerHTML == "C") {
+            setScreen1('');
+        }
+        else if (thisEle.innerHTML == "Del") {
+            const value = getScreen1();
+            const del = value.substring(0, value.length - 1);
+            setScreen1(del);
+        }
+        else {
+            const OnScreen1 = getScreen1();
+            const NewScreen1 = OnScreen1 + thisEle.innerText;
+            num1 = NewScreen1;
+            setScreen1(NewScreen1);
+        }
+
     })
 })
 
 
 alert('This project is still on development process')
+alert('still some bugs remaining')
